cf9cb84f14400613a515807dcf2f86a31d44a092
khrisnakamarga <khrisna@uw.edu>
2019-03-07 11:29:50 -0800

    added automation script

---
+++ automate_HW4.m
@@ -0,0 +1,20 @@
+% Khrisna Kamarga
+% AMATH482 - HW4 Automate
+close all; clear all; clc;
+folder = 1;
+save automate
+Kamarga_AMATH482_HW4
+save log_test1
+
+close all; clear all; clc;
+folder = 2;
+save automate
+Kamarga_AMATH482_HW4
+save log_test2
+
+close all; clear all; clc;
+folder = 3;
+save automate
+Kamarga_AMATH482_HW4
+save log_test3
+close all; clear all; clc;
